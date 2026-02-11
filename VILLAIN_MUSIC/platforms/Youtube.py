f7deda3ad7822c84f408cc19cff5c5155bcf1b25
soni30141241 <soni30141241@gmail.com>
2026-02-11 18:15:39 +0530

    Update Youtube.py

--- VILLAIN_MUSIC/platforms/Youtube.py
+++ VILLAIN_MUSIC/platforms/Youtube.py
@@ -15,9 +15,9 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from py_yt import VideosSearch
 import base64
-from AnonXMusic import LOGGER
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from VILLAIN_MUSIC import LOGGER
+from VILLAIN_MUSIC.utils.database import is_on_off
+from VILLAIN_MUSIC.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
 logger = LOGGER(__name__)
@@ -634,3 +634,4 @@ class YouTubeAPI:
             downloaded_file = await audio_dl(vid_id)
         
         return downloaded_file, direct
+
