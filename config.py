e3ee8b8b04bf93964f05060b8e407e50a6fb2706
soni30141241 <soni30141241@gmail.com>
2026-02-11 17:40:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,6 +42,10 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+# Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
+YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/soni30141241/Queenbot",
